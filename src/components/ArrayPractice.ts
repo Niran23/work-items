87815d5b4537491dbc51548df4cf8550c5fe554f
Niran23 <niranjani.0823@gmail.com>
2026-09-23 11:38:11 +0530

    array methods

---
+++ src/components/ArrayPractice.ts
@@ -0,0 +1,109 @@
+/* const student = {
+  name: "Niranjani",
+  age: 21,
+};
+console.log(student.name); */
+
+
+//with json
+/* const student = {
+  name: "Niranjani",
+  age: 21,
+};
+const jsonText = JSON.stringify(student);
+console.log(jsonText); */
+
+
+// length
+/* const fruits = ["Apple", "Mango", "Banana", "Orange", "Grapes"];
+console.log(fruits.length)
+console.log(fruits[0])
+console.log(fruits[2]) */
+
+
+// run comment npx tsx src/components/ArrayPractice.ts
+
+
+/* const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits[fruits.length-2]); */
+
+
+//using push and pop 
+/* const fruits = ["Apple", "Mango","orange"];
+
+console.log(fruits.length);
+
+fruits.pop("Orange");
+
+console.log(fruits.length); */
+
+
+//loop 
+/* const fruits = ["Apple", "Mango", "Orange"];
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+} */
+
+  //length with number of letters
+  /* const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits[0], fruits[0].length); */
+
+//withString
+/* const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.toString());
+ */
+//without 
+/* const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits); */
+/* const number = 123;
+console.log(number.toString()); */
+
+//arrayat
+/* const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.at(1)); */
+
+//join
+/* const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.join(" - ")); */
+
+//push
+/* const fruits = ["Banana", "Orange"];
+fruits.push("Apple");
+console.log(fruits); */
+
+//shift
+/* const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.shift();
+console.log(fruits); */
+
+//unshift
+/* const fruits = ["Apple", "Mango"];
+const result = fruits.unshift("Banana");
+console.log(fruits); */
+
+//arrayisarray
+/* const data = 123;
+console.log(Array.isArray(data))  */
+
+//delete
+/* 
+const fruits = ["Apple", "Mango", "Banana", "Orange"];
+delete fruits[1];
+console.log(fruits); */
+
+//splice
+/* const fruits = ["Apple", "Mango", "Banana", "Orange"];
+ fruits.splice(1,2);
+console.log(fruits);
+ */
+
+//concat
+/* const fruits1 = ["Apple", "Mango"];
+const fruits2 = ["Banana", "Orange"];
+const fruits = fruits1.concat(fruits2);
+console.log(fruits); */
+
+/* const numbers = ["A","B","C","D","E"];
+numbers.copyWithin(1, 3);
+console.log(numbers); */
